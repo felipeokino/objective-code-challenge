9d300ec9b29e9c1624fde375040334761c4d1c73
FelipeOkino <felipe1okino@live.com>
2023-10-06 14:50:35 -0300

    Style/responsive improvements (#6)
    * style: added more responsive breakpoint in list heroes
    
    * style: improve pagination size on large screen

--- src/utils/index.spec.ts
+++ src/utils/index.spec.ts
@@ -1,12 +1,47 @@
-import { expect, test } from 'vitest';
+import { expect, test, vi } from 'vitest';
 
-import { generatePaginationList } from '.';
+import { generatePaginationList, getSearchParams } from '.';
+
+const mockAssign = vi.fn()
+
+const makeSut = () => {
+    window = Object.create(window);
+    const url = "http://localhost:5174/?page=1&q=mock+search";
+    Object.defineProperty(window, 'location', {
+    value: {
+        href: url,
+        search: '?page=1&q=mock search',
+        assign: mockAssign
+    },
+    writable: true
+    });
+}
 
 test.each([
-    [1, 20, [1, 2, '...', 20]],
-    [21, 30, [21, 22, '...', 30]],
-    [15, 20, [15, 16, '...', 20]],
-    [17, 20, [17, 18, 19, 20]],
+    [1, 20, [ 1, 2, 3, 4, 5, '...', 20 ]],
+    [21, 30, [ 21, 22, 23, 24, 25, '...', 30 ]],
+    [15, 20, [ 14, 15, 16, 17, 18, 19, 20 ]],
+    [17, 20, [ 14, 15, 16, 17, 18, 19, 20 ]],
 ])('should generate correct pagination list', (startAt, size, expected) => {
     expect(generatePaginationList(startAt, size)).toEqual(expected);
 });
+
+
+test.each([
+    [1, 20, [ 1, 2, '...', 20 ]],
+    [21, 30, [ 21, 22, '...', 30 ]],
+    [15, 20, [ 15, 16, '...', 20 ]],
+    [17, 20, [ 17, 18, 19, 20 ]],
+])('should generate correct pagination list in mobile', (startAt, size, expected) => {
+    window.innerWidth = 500;
+    window.dispatchEvent(new Event('resize'));
+    expect(generatePaginationList(startAt, size)).toEqual(expected);
+});
+
+test('should get search params', () => {
+    makeSut()
+
+    const searchParams = getSearchParams()
+
+    expect(searchParams).toEqual('q=mock search')
+})
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,20 +1,27 @@
 export const getActualPage = (): number => +(window.location.search.match(/\d+/g)?.[0]||1)
 export const getSearchParams = (): string => window.location.search.match(/q=([a-z]|[A-Z]).*/)?.[0] || ''
 
+enum PaginationStep {
+  MOBILE = 3,
+  OTHER = 6
+}
 export const generatePaginationList = (startAt: number, size = 1) => {
+  const isMobile = window.innerWidth <= 640;
+  
     const startIndex = 0;
-    let paginationSize = 3;
+    let paginationSize = isMobile ? PaginationStep.MOBILE : PaginationStep.OTHER;
+
     if (size < paginationSize)
       paginationSize = size === 1 ? size-1 : size;
     
-    const homes: Array<number | string> = [];
-    homes.length = paginationSize;
+    const steps: Array<number | string> = [];
+    steps.length = paginationSize;
         
     const pages =  [ 
-      ...homes.fill(0, startIndex, paginationSize)
+      ...steps.fill(0, startIndex, paginationSize)
         .splice(startIndex, paginationSize)
         .map((_, idx) => startAt + idx+1 <= size ? startAt + idx : startAt-=1)
-        .sort((a, b) => a - b), ...homes ];
+        .sort((a, b) => a - b), ...steps ];
         
     if (size - startAt > paginationSize)
       pages[paginationSize-1] = '...';
