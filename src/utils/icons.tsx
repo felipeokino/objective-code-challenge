af0dc511a933173f5562cbdd351de2a0bdfc431a
Felipe Okino <felipe1okino@live.com>
2023-10-06 05:36:12 -0300

    feat: added search and pagination components

---
+++ src/utils/icons.tsx
@@ -0,0 +1,9 @@
+import Search from '../assets/icons/search';
+import ArrowLeft from '../assets/icons/arrowLeft';
+import ArrowRight from '../assets/icons/arrowRight';
+
+export {
+    Search,
+    ArrowLeft,
+    ArrowRight
+}
